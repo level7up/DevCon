ad944f2074a65de7cb733b146f7b04b654eb960a
level7up <abdoleemos200@gmail.com>
2019-09-23 00:43:43 +0200

    inital express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://level:level@node-torkn.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Mongo DB Connected!'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+// User Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => console.log(`Server Running on port 127.0.0.1:${port}`));
