065d6c085b5fb2e19f37fe7175e0205e1120cf89
level7up <abdoleemos200@gmail.com>
2019-09-23 13:01:44 +0200

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+// load User Model 
+const User = require('../models/User');
 
 // @route GET api/users/test
 
@@ -7,8 +9,33 @@ const router = express.Router();
 
 // @access  Public
 
-
 router.get('/test', (req, res) => res.json({
     msg: 'Users Works'
 }));
+
+// @route GET api/users/register
+
+// @desc Registeriton
+
+// @access  Public
+router.post('/register', (req, res) => {
+    User.findOne({
+            email: req.body.email
+        })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({
+                    email: 'Email Exist'
+                });
+            } else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar
+                });
+            }
+        });
+});
+
 module.exports = router;
