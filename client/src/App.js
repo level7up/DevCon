53f80865aaa2ccc9d5dc8410242b4ea80212260d
level7up <abdoleemos200@gmail.com>
2020-02-01 00:46:15 +0200

    1-2-2020

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authAction";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 import store from "./store";
 import Navbar from "./components/layout/Navbar";
@@ -11,6 +12,7 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard";
 
 import "./App.css";
 
@@ -27,8 +29,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout User
     store.dispatch(logoutUser());
-    // TODO:: Clear current profile
-
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
     // redirect to login
     window.location.href = "/login";
   }
@@ -45,6 +47,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/login" component={Login}></Route>
               <Route exact path="/register" component={Register}></Route>
+              <Route exact path="/dashboard" component={Dashboard}></Route>
             </div>
             <Footer />
           </div>
